5f9c4c086d55a786daa0b0afea877dfa13de6397
Faissal Elamraoui <amr.faissal@gmail.com>
2019-03-29 07:31:10 +0100

    Add time series data structure

---
+++ time_series.go
@@ -0,0 +1,105 @@
+package anomalia
+
+import (
+	"sort"
+)
+
+type TimeSeries struct {
+	Timestamps []float64
+	Values     []float64
+}
+
+// NewTimeSeries creates a new time series data structure
+func NewTimeSeries(timestamps []float64, values []float64) *TimeSeries {
+	if len(timestamps) != len(values) {
+		panic("timestamps and values must have the same size")
+	}
+	return &TimeSeries{
+		Timestamps: timestamps,
+		Values:     values,
+	}
+}
+
+// EarliestTimestamp returns the earliest timestamp in the time series
+func (ts *TimeSeries) EarliestTimestamp() float64 {
+	min, _ := MinMax(ts.Timestamps)
+	return min
+}
+
+// LastestTimestamp returns the latest timestamp in the time series
+func (ts *TimeSeries) LastestTimestamp() float64 {
+	_, max := MinMax(ts.Timestamps)
+	return max
+}
+
+// Zip convert the time series to a map (map[Timestamp]Value)
+func (ts *TimeSeries) Zip() map[float64]float64 {
+	m := make(map[float64]float64)
+	sort.Slice(ts.Timestamps, func(i, j int) bool {
+		return ts.Timestamps[i] < ts.Timestamps[j]
+	})
+	for idx, timestamp := range ts.Timestamps {
+		m[timestamp] = ts.Values[idx]
+	}
+	return m
+}
+
+// AddOffset increments time series timestamps by some offset
+func (ts *TimeSeries) AddOffset(offset float64) *TimeSeries {
+	offsettedTimestamps := Map(ts.Timestamps, func(timestamp float64) float64 { return timestamp + offset })
+	return NewTimeSeries(offsettedTimestamps, ts.Values)
+}
+
+// Normalize normalizes the time series values by dividing by the maximum value
+func (ts *TimeSeries) Normalize() *TimeSeries {
+	_, max := MinMax(ts.Values)
+	normalizedValues := Map(ts.Values, func(value float64) float64 { return value / max })
+	return NewTimeSeries(ts.Timestamps, normalizedValues)
+}
+
+// NormalizeWithMinMax normalizes time series values using MixMax
+func (ts *TimeSeries) NormalizeWithMinMax() *TimeSeries {
+	normalizedValues := ts.Values
+	if min, max := MinMax(ts.Values); min != max {
+		normalizedValues = Map(ts.Values, func(value float64) float64 { return value - min/max - min })
+	}
+	return NewTimeSeries(ts.Timestamps, normalizedValues)
+}
+
+// Crop crops the time series timestamps into the specified range [start, end]
+func (ts *TimeSeries) Crop(start, end float64) *TimeSeries {
+	zippedSeries := ts.Zip()
+	// Filter timestamps within the crop range
+	timestamps := Filter(ts.Timestamps, func(timestamp float64) bool {
+		return (timestamp >= start) && (timestamp <= end)
+	})
+
+	// Get values of cropped timestamps
+	values := make([]float64, 0, len(timestamps))
+	for _, timestamp := range timestamps {
+		values = append(values, zippedSeries[timestamp])
+	}
+	return NewTimeSeries(timestamps, values)
+}
+
+// Average calculates average value over the time series
+func (ts *TimeSeries) Average() float64 {
+	total := 0.0
+	values := CopySlice(ts.Values)
+	for _, v := range values {
+		total += v
+	}
+	return total / float64(len(values))
+}
+
+// Median calculates median value over the time series.
+func (ts *TimeSeries) Median() float64 {
+	sorted := SortedCopy(ts.Values)
+	len := len(sorted)
+	mid := len / 2
+
+	if len%2 == 0 {
+		return (sorted[mid-1] + sorted[mid]) / 2
+	}
+	return sorted[mid]
+}
---
+++ time_series_test.go
@@ -0,0 +1,108 @@
+package anomalia
+
+import (
+	"reflect"
+	"testing"
+)
+
+var (
+	timestamps = []float64{2, 3, 5, 8, 9, 10, 15}
+	values     = []float64{1.0, 0.6, 2.5, 1.9, 0.3, 3.2, 0}
+)
+
+func TestNewTimeSeries(t *testing.T) {
+	ts := NewTimeSeries(timestamps, values)
+	actualType := "*anomalia.TimeSeries"
+	expectedType := reflect.TypeOf(ts).String()
+	if expectedType != actualType {
+		t.Fatalf("expected '%s', got '%s'", expectedType, actualType)
+	}
+
+	// Assert panic
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("NewTimeSeries did not panic")
+		}
+	}()
+	NewTimeSeries([]float64{1, 2}, []float64{1})
+}
+
+func TestEarliestTimestamp(t *testing.T) {
+	ts := NewTimeSeries(timestamps, values)
+	actual := ts.EarliestTimestamp()
+	expected := 2.0
+	if actual != expected {
+		t.Fatalf("expected '%v', got '%v'", expected, actual)
+	}
+}
+
+func TestLastestTimestamp(t *testing.T) {
+	ts := NewTimeSeries(timestamps, values)
+	actual := ts.LastestTimestamp()
+	expected := 15.0
+	if actual != expected {
+		t.Fatalf("expected '%v', got '%v'", expected, actual)
+	}
+}
+
+func TestZipTimeSeries(t *testing.T) {
+	ts := NewTimeSeries(timestamps, values)
+	expected := ts.Zip()
+	if (len(expected) != len(ts.Timestamps)) || (len(expected) != len(ts.Values)) {
+		t.Fatalf("time series lengths do not match")
+	}
+}
+
+func TestAddOffsetToTimeSeries(t *testing.T) {
+	ts := NewTimeSeries(timestamps, values)
+	offsetted := ts.AddOffset(1)
+	if len(ts.Timestamps) != len(offsetted.Timestamps) {
+		t.Fatalf("offsetted time series length do not match")
+	}
+}
+
+func TestNormalize(t *testing.T) {
+	ts := NewTimeSeries(timestamps, values).Normalize()
+	if ts == nil {
+		t.Fatalf("normalized time series cannot be nil")
+	}
+}
+
+func TestNormalizeWithMinMax(t *testing.T) {
+	ts := NewTimeSeries(timestamps, values).NormalizeWithMinMax()
+	if ts == nil {
+		t.Fatalf("minMax normalized time series cannot be nil")
+	}
+}
+
+func TestCrop(t *testing.T) {
+	ts := NewTimeSeries(timestamps, values).Crop(0, 4)
+	if len(ts.Timestamps) != 2 || len(ts.Values) != 2 {
+		t.Fatalf("expected size to be 2, got %v", len(ts.Timestamps))
+	}
+}
+
+func TestAverage(t *testing.T) {
+	actual := NewTimeSeries(timestamps, values).Average()
+	expected := float64(0.424107)
+	if actual != expected {
+		t.Fatalf("expected %f, got %f", expected, actual)
+	}
+}
+
+func TestMedian(t *testing.T) {
+	ts := NewTimeSeries(timestamps, values)
+
+	actual := ts.Median()
+	expected := 0.312500
+	if expected != actual {
+		t.Fatalf("expected %f, got %f", expected, actual)
+	}
+
+	ts = ts.Crop(0, 8)
+	actual = ts.Median()
+	expected = 0.312500
+	if expected != actual {
+		t.Fatalf("expected %f, got %f", expected, actual)
+	}
+}
