69cd47db467b339e235e3b291fea7444bf2eaf1d
Faissal Elamraoui <amr.faissal@gmail.com>
2019-04-09 08:35:12 +0200

    Ability to zip ScoreList to map[float64]float64

--- score_list.go
+++ score_list.go
@@ -8,6 +8,17 @@ type ScoreList struct {
 	Scores     []float64
 }
 
+// Zip convert the score list to map (map[Timestamp]Score)
+func (sl *ScoreList) Zip() map[float64]float64 {
+	m := make(map[float64]float64)
+	sorted := sortedCopy(sl.Timestamps)
+
+	for idx, timestamp := range sorted {
+		m[timestamp] = sl.Scores[idx]
+	}
+	return m
+}
+
 // Denoise sets low(noisy) scores to 0.0
 func (sl *ScoreList) Denoise() *ScoreList {
 	_, max := minMax(sl.Scores)
