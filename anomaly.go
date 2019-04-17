f8dd904138b9d48179730c886dd48c319d4f9960
Faissal Elamraoui <amr.faissal@gmail.com>
2019-04-17 15:46:25 +0200

    Get anomaly timestamped score

--- anomaly.go
+++ anomaly.go
@@ -2,6 +2,7 @@ package anomalia
 
 // Anomaly holds information about the detected anomaly/outlier
 type Anomaly struct {
+	Timestamp      float64
 	StartTimestamp float64
 	EndTimestamp   float64
 	Score          float64
@@ -13,4 +14,9 @@ type Anomaly struct {
 // GetTimeWindow returns anomaly start and end timestamps
 func (anomaly *Anomaly) GetTimeWindow() (float64, float64) {
 	return anomaly.StartTimestamp, anomaly.EndTimestamp
+}
+
+// GetTimestampedScore returns anomaly exact timestamp with calculated score
+func (anomaly *Anomaly) GetTimestampedScore() (float64, float64) {
+	return anomaly.Timestamp, anomaly.Score
 }
