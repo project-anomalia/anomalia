2fcc75b044412c9b5625d853cb5b3fec8808a57b
Faissal Elamraoui <amr.faissal@gmail.com>
2019-04-22 15:33:27 +0200

    Add NewAbsoluteThreshold constructor

--- absolute_threshold.go
+++ absolute_threshold.go
@@ -1,10 +1,18 @@
 package anomalia
 
+// AbsoluteThreshold holds absolute threshold algorithm configuration.
+// It takes the difference of lower and upper thresholds with the current value as anomaly score.
 type AbsoluteThreshold struct {
 	LowerThreshold float64
 	UpperThreshold float64
 }
 
+// NewAbsoluteThreshold returns AbsoluteThAbsoluteThreshold instance
+func NewAbsoluteThreshold(lower, upper float64) *AbsoluteThreshold {
+	return &AbsoluteThreshold{lower, upper}
+}
+
+// Run runs the absolute threshold algorithm over the time series
 func (at *AbsoluteThreshold) Run(timeSeries *TimeSeries) *ScoreList {
 	scoreList, _ := at.computeScores(timeSeries)
 	return scoreList
@@ -20,9 +28,6 @@ func (at *AbsoluteThreshold) computeScores(timeSeries *TimeSeries) (*ScoreList, 
 			return 0.0
 		}
 	})
-	scoreList := &ScoreList{
-		Timestamps: timeSeries.Timestamps,
-		Scores:     scores,
-	}
+	scoreList := &ScoreList{timeSeries.Timestamps, scores}
 	return scoreList, nil
 }
